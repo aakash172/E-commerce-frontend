ea0aa56f9ea47cec487a62842a15714519bc2ec7
aakash <aakashchaudhary172m@gmail.com>
2024-09-13 01:45:52 +0530

    login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 function Header() {
     return (
-        <header className='h-16 shadow-md'>
+        <header className='h-16 shadow-md bg-white'>
             <div className="h-full container mx-auto flex items-center px-4 justify-between">
                 <div className="">
-                    <Logo w={90} h={50} />
+                    <Link to={'/'}>
+                        <Logo w={90} h={50} />
+                    </Link>
+
                 </div>
 
                 <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2'>
@@ -16,10 +21,21 @@ function Header() {
                     </div>
                 </div>
 
-                <div>
-                    <div>
+                <div className='flex items-center gap-7'>
+                    <div className='text-3xl cursor-pointer'>
                         <FaRegCircleUser />
                     </div>
+
+                    <div className='text-2xl relative'>
+                        <span><FaShoppingCart /></span>
+                        <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3 '>
+                            <p className='text-sm'>0</p>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <Link to={'/login'} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-800'>login</Link>
+                    </div>
+
                 </div>
             </div>
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "./../pages/Home"
+import Login from "../pages/Login";
+import ForgotPassword from "../pages/ForgotPassword";
 
 const router = createBrowserRouter([
     {
@@ -9,6 +11,14 @@ const router = createBrowserRouter([
         children: [{
             path: "",
             element: <Home />
+        },
+        {
+            path: "login",
+            element: <Login />
+        },
+        {
+            path: "forgot-password",
+            element: <ForgotPassword />
         }
         ]
     }
