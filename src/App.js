34a7351b4306d532cf45eabeb0646e264c2b6f03
aakash <aakashchaudhary172m@gmail.com>
2024-09-26 21:38:14 +0530

    addedUserDetails

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import "react-toastify/dist/ReactToastify.css";
 import SummaryApi from "./common";
 import { useEffect } from "react";
 import Context from "./context";
+import { useDispatch } from "react-redux";
+import { setUserDetails } from "./store/usersSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const fetchUserDetails = async () => {
     const dataResponse = await fetch(SummaryApi.current_user.url, {
       method: SummaryApi.current_user.method,
       credentials: "include",
     });
-    const dataApi = dataResponse.json();
+    const dataApi = await dataResponse.json();
+    if (dataApi.success) {
+      dispatch(setUserDetails(dataApi.data));
+    }
   };
   useEffect(() => {
     fetchUserDetails();
