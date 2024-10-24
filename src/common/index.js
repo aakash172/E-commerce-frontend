85ec538c1707326d9bfa9ef2aafc722bfedd32d5
aakash <aakashchaudhary172m@gmail.com>
2024-10-25 00:30:10 +0530

    deploy

--- src/common/index.js
+++ src/common/index.js
@@ -1,80 +1,78 @@
-const backendDomain = "https://shopit-aakash-172.vercel.app/";
-
 const SummaryApi = {
   SignUP: {
-    url: `${backendDomain}/api/signup`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/signup`,
     method: "post",
   },
   SignIn: {
-    url: `${backendDomain}/api/signin`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/signin`,
     method: "post",
   },
   current_user: {
-    url: `${backendDomain}/api/user-details`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/user-details`,
     method: "get",
   },
   logout_user: {
-    url: `${backendDomain}/api/userLogout`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/userLogout`,
     method: "get",
   },
   allUser: {
-    url: `${backendDomain}/api/all-user`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/all-user`,
     method: "get",
   },
   updateUser: {
-    url: `${backendDomain}/api/update-user`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/update-user`,
     method: "post",
   },
   uploadProduct: {
-    url: `${backendDomain}/api/upload-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/upload-product`,
     method: "post",
   },
   allProduct: {
-    url: `${backendDomain}/api/get-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/get-product`,
     method: "get",
   },
   updateProduct: {
-    url: `${backendDomain}/api/update-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/update-product`,
     method: "post",
   },
   categoryProduct: {
-    url: `${backendDomain}/api/get-categoryProduct`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/get-categoryProduct`,
     method: "get",
   },
   categoryWiseProduct: {
-    url: `${backendDomain}/api/category-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/category-product`,
     method: "post",
   },
   productDetails: {
-    url: `${backendDomain}/api/product-details`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/product-details`,
     method: "post",
   },
   addToCartProduct: {
-    url: `${backendDomain}/api/addToCart`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/addToCart`,
     method: "post",
   },
   addToCartProductCount: {
-    url: `${backendDomain}/api/countAddToCartProduct`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/countAddToCartProduct`,
     method: "get",
   },
   addToCartProductView: {
-    url: `${backendDomain}/api/view-cart-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/view-cart-product`,
     method: "get",
   },
   updateCartProduct: {
-    url: `${backendDomain}/api/update-cart-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/update-cart-product`,
     method: "post",
   },
   deleteCartProduct: {
-    url: `${backendDomain}/api/delete-cart-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/delete-cart-product`,
     method: "post",
   },
   searchProduct: {
-    url: `${backendDomain}/api/search`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/search`,
     method: "get",
   },
   filterProduct: {
-    url: `${backendDomain}/api/filter-product`,
+    url: `${process.env.REACT_APP_BACKEND_URL}/api/filter-product`,
     method: "post",
   },
 };
